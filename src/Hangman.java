6ea02b6a0d3ccbcdb25849fca2543fa711468952
Byasar3 <bernayasar@hotmail.co.uk>
2024-05-14 16:53:08 +0100

    created Word class

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,29 +1,65 @@
 import java.util.Scanner;
-
+// class for connecting the other classes together and running the game
 public class Hangman {
 
     // states and behaviours of hangman game / blueprint to create instances with
-    // each game is new instance
-    String word;
+    protected Word word;
+    protected String wordToGuess;
+    protected int lives;
+    protected char[] lettersGuessed;
 
-    // class for connecting the other classes together and running the game
+    // constructor
+    public Hangman() {
+        this.word = new Word();
+        this.wordToGuess = word.getRandomWord();
+        this.lives = 10; // hard coded number of lives
+    }
+
+    // getters and setters
+    public Word getWord() {
+        return word;
+    }
+
+    public void setWord(Word word) {
+        this.word = word;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    public char[] getLettersGuessed() {
+        return lettersGuessed;
+    }
+
+    public void setLettersGuessed(char[] lettersGuessed) {
+        this.lettersGuessed = lettersGuessed;
+    }
+
+    // methods
+    public void startGame () {
+        Scanner newScannerObject = new Scanner(System.in);
+        //click any key to start game
+        System.out.println("Press the enter key to start the game!");
+        newScannerObject.nextLine();
+
+        System.out.println(wordToGuess);
+    }
+
+    // needs to get word and display word
+    // then needs to ask for user input,
+    // save that input into guessed letters array,
+    // check if the guessed letter is in word, if so, say congrats, put in word and render filled in word
+    // if not in word, say sorry not in word and -1 life
+
 
     public static void main(String[] args) {
-
-        // i think this will go above main, and main will only call it/things it needs
-
-        Scanner newScannerObject = new Scanner(System.in);
-
-        //click any button to start game
-        System.out.println("Press any key to start game!");
-        String startGame = newScannerObject.nextLine();
-
-        // needs to get word and display word
-        System.out.println("working check: " + startGame);
-
-        // then needs to ask for user input,
-        // save that input into guessed letters array,
-        // check if the guessed letter is in word, if so, say congrats, put in word and render filled in word
-        // if not in word, say sorry not in word and -1 life
+    // create an instance of hangman and run startgame method on it
+    Hangman game = new Hangman();
+    game.startGame();
     }
 }
--- src/Word.java
+++ src/Word.java
@@ -5,5 +5,32 @@ public class Word {
     private String[] words = {"elephant", "java", "coding", "cat", "dog"};
     private String currentWord;
 
+    // constructor
+    public Word(){
+        this.currentWord = getRandomWord(); // will get word from get random word method
+    }
+
+    // getters and setters
+    public String[] getWords() {
+        return words;
+    }
+
+    public void setWords(String[] words) {
+        this.words = words;
+    }
+
+    public String getCurrentWord() {
+        return currentWord;
+    }
+
+    public void setCurrentWord(String currentWord) {
+        this.currentWord = currentWord;
+    }
+
     // method to get random word
+    public String getRandomWord () {
+        return words[(int)(Math.random() * words.length)];
+    }
+
+    // turn word into underscores - here? or in Hangman class?
 }
