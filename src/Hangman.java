b6f62e67f2493ebdbea52f816c9da108cafef7a7
Byasar3 <bernayasar@hotmail.co.uk>
2024-05-15 17:03:11 +0100

    ResultsDisplay class incorporated

--- src/Hangman.java
+++ src/Hangman.java
@@ -6,9 +6,11 @@ public class Hangman {
     // states and behaviours of hangman game / blueprint to create instances with
     protected Word word;
     protected String wordToGuess;
+    protected char[] wordToGuessUnderscore;
     protected int lives;
     protected char[] lettersGuessed;
-    protected char[] wordToGuessUnderscore;
+    protected char guessedLetter;
+    protected ResultsDisplay resultsDisplay;
 
     // constructor
     public Hangman() {
@@ -16,31 +18,7 @@ public class Hangman {
         this.wordToGuess = word.getRandomWord();
         this.lives = 10; // hard coded number of lives
         this.wordToGuessUnderscore = turnWordIntoUnderscores(wordToGuess);
-    }
-
-    // getters and setters
-    public Word getWord() {
-        return word;
-    }
-
-    public void setWord(Word word) {
-        this.word = word;
-    }
-
-    public int getLives() {
-        return lives;
-    }
-
-    public void setLives(int lives) {
-        this.lives = lives;
-    }
-
-    public char[] getLettersGuessed() {
-        return lettersGuessed;
-    }
-
-    public void setLettersGuessed(char[] lettersGuessed) {
-        this.lettersGuessed = lettersGuessed;
+        this.resultsDisplay = new ResultsDisplay();
     }
 
     // methods
@@ -54,28 +32,32 @@ public class Hangman {
         return underscoredWord.toString().trim().toCharArray();
     }
 
-    public void startGame () {
-        Scanner newScannerObject = new Scanner(System.in);
-        //click any key to start game
-        System.out.println("Press the enter key to start the game!");
-        newScannerObject.nextLine();
+    public void playGame() {
+            resultsDisplay.displayGameStart();
+            resultsDisplay.displayWord(wordToGuessUnderscore);
+            handleGuess();
+            resultsDisplay.displayLives(lives);
 
-        System.out.println(wordToGuess);
-        System.out.println(wordToGuessUnderscore);
+    }
+
+    public void handleGuess() {
+        Scanner newScannerObject = new Scanner(System.in);
+        System.out.println("Guess a letter! ");
+        // check if letter already been guessed
+        //
+
     }
 
 
 
-    // needs to get word and display word
-    // then needs to ask for user input,
     // save that input into guessed letters array,
     // check if the guessed letter is in word, if so, say congrats, put in word and render filled in word
     // if not in word, say sorry not in word and -1 life
 
 
     public static void main(String[] args) {
-    // create an instance of hangman and run startgame method on it
+    // create an instance of hangman and run playgame method on it
     Hangman game = new Hangman();
-    game.startGame();
+    game.playGame();
     }
 }
--- src/ResultsDisplay.java
+++ src/ResultsDisplay.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class ResultsDisplay {
     // class for displaying results
+    public void displayGameStart() {
+        Scanner newScannerObject = new Scanner(System.in);
+        //click any key to start game
+        System.out.println("Press the enter key to start the game!");
+        newScannerObject.nextLine();
+    }
 
     // get the randomised word and display it
+    public void displayWord(char[] wordToGuessUnderscore){
+        System.out.println(wordToGuessUnderscore);
+    }
 
-    // update the word with each letter guess
+    // display the word with each letter guess
+    // is this needed? could just update the word to guess underscore in hangman...
 
-    // update the list of letters guessed
-
-    // update lives left/hanging man
+    // display lives left/hanging man
+    public void displayLives(int lives) {
+        System.out.println("You have " + lives + " lives left.");
+    }
 }
