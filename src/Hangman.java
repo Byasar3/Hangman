3f2e6f4d8bb2dbd284798be9e94644f1ae8d351a
Byasar3 <bernayasar@hotmail.co.uk>
2024-05-16 17:52:17 +0100

    checking user input vs inputted chars

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,13 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 // class for connecting the other classes together and running the game
 public class Hangman {
 
-    // states and behaviours of hangman game / blueprint to create instances with
+    // states
     protected Word word;
     protected String wordToGuess;
     protected char[] wordToGuessUnderscore;
     protected int lives;
-    protected char[] lettersGuessed;
+    protected ArrayList<Character> lettersGuessed;
     protected ResultsDisplay resultsDisplay;
     protected UserInteraction userInteraction;
 
@@ -17,13 +19,13 @@ public class Hangman {
         this.word = new Word();
         this.wordToGuess = word.getRandomWord();
         this.lives = 10; // hard coded number of lives
+        this.lettersGuessed = new ArrayList<>();
         this.wordToGuessUnderscore = turnWordIntoUnderscores(wordToGuess);
         this.resultsDisplay = new ResultsDisplay();
-        this.userInteraction =  new UserInteraction();
+        this.userInteraction = new UserInteraction();
     }
 
     // methods
-
     public char[] turnWordIntoUnderscores(String wordToGuess) {
         // Create a StringBuilder to build the underscores with spaces
         StringBuilder underscoredWord = new StringBuilder();
@@ -34,31 +36,43 @@ public class Hangman {
         return underscoredWord.toString().trim().toCharArray();
     }
 
+    public boolean isWordGuessed() {
+        for (char c : wordToGuessUnderscore) {
+            if (c == '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void handleGuess(char guessedLetter) {
+        // check if letter already been guessed
+        if (!lettersGuessed.contains(guessedLetter)) {
+            lettersGuessed.add(guessedLetter);
+        } else {
+            System.out.println("You already guessed " + guessedLetter + " letter");
+        }
+
+    }
+
+    // check if the guessed letter is in word, if so, say congrats,
+    // put in word and render filled in word
+    // if not in word, say sorry not in word and -1 life
+
     public void playGame() {
-            resultsDisplay.displayGameStart();
+        resultsDisplay.displayGameStart();
+        while (lives > 0 && !isWordGuessed()) {
             resultsDisplay.displayWord(wordToGuessUnderscore);
             resultsDisplay.displayLives(lives);
             Scanner newScannerObject = new Scanner(System.in);
             char guessedLetter = userInteraction.getUserGuess(newScannerObject);
             handleGuess(guessedLetter);
+        }
     }
-
-    public void handleGuess(char guessedLetter) {
-        System.out.println("this be here: " + guessedLetter);
-        // check if letter already been guessed
-        //
-    }
-
-
-
-    // save that input into guessed letters array,
-    // check if the guessed letter is in word, if so, say congrats, put in word and render filled in word
-    // if not in word, say sorry not in word and -1 life
-
 
     public static void main(String[] args) {
-    // create an instance of hangman and run playgame method on it
-    Hangman game = new Hangman();
-    game.playGame();
+        // create an instance of hangman and run playgame method on it
+        Hangman game = new Hangman();
+        game.playGame();
     }
 }
--- src/ResultsDisplay.java
+++ src/ResultsDisplay.java
@@ -5,17 +5,23 @@ public class ResultsDisplay {
     public void displayGameStart() {
         Scanner newScannerObject = new Scanner(System.in);
         //click any key to start game
+        displayRules();
         System.out.println("Press the enter key to start the game!");
         newScannerObject.nextLine();
+    }
+
+    // maybe include rules here = not allowed to guess word, only allowed to guess char
+    public void displayRules() {
+        System.out.println("   RULES:  ");
+        System.out.println("1. You can only guess one letter at a time, not a word.");
+        System.out.println("2. Each wrong letter guess is one life lost.");
+        System.out.println("3. The game will end when you're out of lives or the word is guessed.");
     }
 
     // get the randomised word and display it
     public void displayWord(char[] wordToGuessUnderscore){
         System.out.println(wordToGuessUnderscore);
     }
-
-    // display the word with each letter guess
-    // is this needed? could just update the word to guess underscore in hangman...
 
     // display lives left/hanging man
     public void displayLives(int lives) {
